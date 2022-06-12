a4408af17f945a03f1b5d7358c0e4b26d0efdec3
Jose Pablo <sortzweb@gmail.com>
2022-06-12 20:28:33 +0200

    Entrar

--- CreadorPoemasHorribles.py
+++ CreadorPoemasHorribles.py
@@ -12,8 +12,33 @@ adverbios =["malamente","rapidamente","mal","regular"]
 #   -Articulo + Sujeto + Verbo + Adverbio
 #   -Articulo +sujeto + verbo
 
+
+#To make the awful poetry program more versatile, add some code to it so that if the user enters a 
+# number on the command line (between 1 and 10 inclusive), the program will output that many lines.
+#If no command-line argument is given, default to printing five lines as before. 
+def get_int(msg,default):
+    while True:
+        try:    
+            i=input(msg)
+            if not i:
+                return default
+            else:
+                num= int(i)
+                if num >10 or num <0:
+                    print("No puede ser")
+                else:
+                    return num
+
+            
+        except ValueError as error1:
+            print(error1)
+
+
+
 i=0
-while i <5:
+hasta=get_int("Introduzca el numero de lineas entre 0 o 10 (Enter para 5): ",5 )
+
+while i <hasta:
     estructura=random.randint(1,2)
     if estructura ==1:
         frase =random.choice(articulos)+" "+random.choice(sujetos)+" "+random.choice(verbos)+" "+random.choice(adverbios)
